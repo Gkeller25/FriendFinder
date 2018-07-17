a1bdbecf65227b67c3c3d3797f021abcc79854d7
Gkeller25 <kellergaret@outlook.com>
2018-07-17 11:41:23 -0600

    basic test passed. needed: finish html looks , fix links, and clean code

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendData = [
+    {
+        "name":"jim",
+    "photo":"lsadf",
+    "scores":["5","4","3","2","4","5","2","3","1","4"]
+    },
+    {
+    "name":"jeff",
+    "photo":"awer",
+    "scores":["1","3","5","2","3","1","4","2","4","1"]
+    },
+    {
+        "name":"mike",
+        "photo":"awer",
+        "scores":["3","4","1","2","3","5","2","2","4","5"]
+        }
+]
+
+
+
+
+module.exports = friendData;
