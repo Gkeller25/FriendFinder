5a7ad2ef7f1ab17c3ce572b383ac4b4c1e244733
Gkeller25 <kellergaret@outlook.com>
2018-07-17 15:58:12 -0600

    Code cleaned up and placeholder friends created

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,19 @@
 var friendData = [
     {
-        "name":"jim",
-    "photo":"lsadf",
+    "name":"Darth Vader",
+    "photo":"https://www.sideshowtoy.com/wp-content/uploads/2014/07/star-wars-darth-vader-life-size-feature-400184.jpg",
     "scores":["5","4","3","2","4","5","2","3","1","4"]
     },
     {
-    "name":"jeff",
-    "photo":"awer",
+    "name":"bob",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/c/c5/Bob_the_builder.jpg",
     "scores":["1","3","5","2","3","1","4","2","4","1"]
     },
     {
-        "name":"mike",
-        "photo":"awer",
-        "scores":["3","4","1","2","3","5","2","2","4","5"]
-        }
+    "name":"Thor",
+    "photo":"https://static.tvtropes.org/pmwiki/pub/images/mcu_thor.png",
+    "scores":["3","4","1","2","3","5","2","2","4","5"]
+    }
 ]
-
-
-
 
 module.exports = friendData;
